a377acb9c8385c875f4045fa92f6211396bba07a
tomaspineiro1 <t.eiro@campus.fct.unl.pt>
2026-03-27 17:13:13 +0000

    op8

---
+++ src/main/java/pt/unl/fct/di/adc/firstwebapp/util/ChangeUserRoleData.java
@@ -0,0 +1,8 @@
+package pt.unl.fct.di.adc.firstwebapp.util;
+
+public class ChangeUserRoleData {
+    public String username;
+    public String newRole;
+
+    public ChangeUserRoleData() {}
+}
---
+++ src/main/java/pt/unl/fct/di/adc/firstwebapp/util/ChangeUserRoleRequest.java
@@ -0,0 +1,8 @@
+package pt.unl.fct.di.adc.firstwebapp.util;
+
+public class ChangeUserRoleRequest {
+    public ChangeUserRoleData input;
+    public AuthToken token;
+
+    public ChangeUserRoleRequest() {}
+}
